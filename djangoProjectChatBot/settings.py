367fcd1d4cace0979970a2922529fc6914bf99c1
Amirmahdi <amirmahdi.manafi111@gmail.com>
2025-05-30 19:30:21 +0330

    1

--- djangoProjectChatBot/settings.py
+++ djangoProjectChatBot/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
